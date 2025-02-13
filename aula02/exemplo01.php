19e2d8201468a1e94bfb78589b8978f340336e14
LeoTeiSil <161627429+LeoTeiSil@users.noreply.github.com>
2025-02-13 18:23:33 -0300

    Add files via upload

---
+++ aula02/exemplo01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Segunda aula de IW 2025</p>
+
+    <?php
+        $nome= "Gustavo";
+        echo "<p>Oi 2F, sou p Leonardo</p>";
+        echo "<p>Oi 2F, meu nome é $nome</p>";
+        echo "<p>Oi 2F, meu nome é ".$nome."</p>";
+    ?>
+</body>
+</html>
