ebe308955ea66277d15cea69696b9cfc7526782f
LeoTeiSil <161627429+LeoTeiSil@users.noreply.github.com>
2025-02-17 14:59:23 -0300

    Add files via upload

---
+++ aula02/exemplo04.php
@@ -0,0 +1,8 @@
+<?php
+    define('leonardo',1.5);
+    $xyz = 1.5;
+
+    echo "o valor é: ".leonardo;
+    echo "<br>";
+    echo "o valor é: $xyz";
+?>
---
+++ aula02/exemplo06.php
@@ -0,0 +1,14 @@
+<?php
+    $nome = "Leonardo";
+    $idade = 16;
+    $funcao = "Aluno";
+
+    $frase = "Dados pessoais: ";
+    $frase .= $nome;
+    $frase .= " _ ";
+    $frase .= $idade;
+    $frase .= " _ ";
+    $frase .= $funcao;
+
+    echo $frase;
+?>
