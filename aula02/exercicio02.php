12ec7726c6d8e22d60108868b0c5d5fd086be0ea
LeoTeiSil <161627429+LeoTeiSil@users.noreply.github.com>
2025-02-18 21:35:31 -0300

    Add files via upload

--- aula02/exercicio02.php
+++ aula02/exercicio02.php
@@ -1,5 +1,8 @@
 <?php
-    $pi = 3.141592;
+define("PI", 3.141592);
+$raio = 6;
 
-    
+echo "Raio: $raio";
+echo " Área: " . (4 * PI * $raio * $raio);
+echo " Volume: " . ((4 / 3) * PI * $raio * $raio * $raio);
 ?>
