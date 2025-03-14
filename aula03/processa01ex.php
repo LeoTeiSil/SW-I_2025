bcaef6a5741a4c9e3780f505f25308aa962ba3dd
LeoTeiSil <161627429+LeoTeiSil@users.noreply.github.com>
2025-03-14 10:05:49 -0300

    Add files via upload

--- aula03/processa01ex.php
+++ aula03/processa01ex.php
@@ -1,11 +1,7 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST["cxnome"];
     $mensagem = $_POST["cxmensagem"];
 
     echo "Bem vindo, $nome <br><br>";
     echo $mensagem;
-} else {
-    echo "Formulário não submetido.";
-}
 ?>
