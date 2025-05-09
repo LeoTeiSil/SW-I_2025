e339d9415c8a60a555da81e94468e15af8dccdd3
LeoTeiSil <161627429+LeoTeiSil@users.noreply.github.com>
2025-05-09 18:26:42 -0300

    Add files via upload

---
+++ aula09/Lampada.class.php
@@ -0,0 +1,31 @@
+<?php
+    class Lampada{
+        public $Fabricante;
+        public $Tensao;
+        public $Potencia;
+        public $Cor;
+        public $Status;
+
+        public function Ligar(){
+            $this->Status = True;
+        }
+
+        public function Desigar(){
+            $this->Status = False;
+
+        }
+
+        public function MostrarDados(){
+            echo "Fabricante: ". $this->Fabricante."<br>";
+            echo "Tensão: ". $this->Tensao."<br>";
+            echo "Potência: ". $this->Potencia."<br>";
+            echo "Cor: ". $this->Cor."<br>";
+            if ($this->Status == 1){
+                echo "Status da Lâmpada: Ligada <br>";
+            }else{
+              echo "Status da Lâmpada: Desligada";
+            }
+
+        }
+    }
+?>
