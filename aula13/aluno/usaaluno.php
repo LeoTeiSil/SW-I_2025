0e7b243e99d00e83baed617a8fb1276c8620e4e4
LeoTeiSil <161627429+LeoTeiSil@users.noreply.github.com>
2025-06-17 22:26:10 -0300

    Add files via upload

---
+++ aula13/aluno/usaaluno.php
@@ -0,0 +1,19 @@
+<?php
+    include_once 'Aluno.class.php';
+
+    $fulano = new Aluno("Lenardo da Silva",9,7);
+
+    $nota1 = $fulano->getNota1();
+    $nota2 = $fulano->getNota2();
+    
+    // echo $nota1;
+
+    
+    echo "ALUNO:". $aluno = $fulano->getNome()."<br>";
+    $media = $fulano->Media($nota1,$nota2);
+    echo "MÉDIA:". $media . "<br>";
+    echo "SITUAÇÃO:". $fulano->VerificarSituacao($media);
+
+    
+    
+?>
---
+++ aula13/retangulo/usaretangulo.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'retangulo.class.php';
+
+$ret = new Retangulo();
+$ret->setLargura(5);
+$ret->setAltura(3);
+
+echo "Largura: " . $ret->getLargura() . "<br>";
+echo "Altura: " . $ret->getAltura() . "<br>";
+echo "Área: " . $ret->calcularArea() . "<br>";
+echo "Perímetro: " . $ret->calcularPerimetro() . "<br>";
+?>
